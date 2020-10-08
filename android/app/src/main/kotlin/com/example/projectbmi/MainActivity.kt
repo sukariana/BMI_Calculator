633672ca557a389584cad2fc6ad8056df7efae51
sukariana.yasa@gmail.com <agung.ukki.galih@gmail.com>
2020-10-08 08:17:52 +0800

    test

---
+++ android/app/src/main/kotlin/com/example/projectbmi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projectbmi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/about_me.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyProfil extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+      new Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("About Me"),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+              child: Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            image: DecorationImage(image: AssetImage('images/aku.jpg'),
+                                fit: BoxFit.cover
+                            )
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              color: Colors.white,
+              child: Column(
+
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  Text('I Gede Agus Sukariana Yasa',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  SizedBox(height: 5,child: Container(
+                    color: Colors.white,
+                  ),),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('sukariana.yasa@gmail.com',
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black.withOpacity(0.6)
+                        ),
+                      )
+
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 30, right: 50, left: 50),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            //color: Colors.green,
+                            borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+
+                          ),
+
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.my_location,
+                                    color: Colors.blue,
+                                    size: 58),
+                                Text('pupuan',
+                                  style: TextStyle
+                                    (color: Colors.black54,
+                                      fontWeight: FontWeight.bold
+                                  ), )
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            //color: Colors.green,
+                            borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+                          ),
+
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.home, color: Colors.orangeAccent, size: 58,),
+                                Text('pupuan', style: TextStyle
+                                  (color: Colors.black54,
+                                    fontWeight: FontWeight.bold
+                                ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            //color: Colors.green ,
+                            borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+
+
+                          ),
+
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.music_note,color: Colors.purple, size: 58,),
+                                Text('Pop', style: TextStyle
+                                  (color: Colors.black54,
+                                    fontWeight: FontWeight.bold
+                                ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+                          ),
+
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.school, color: Colors.redAccent , size: 58,),
+                                Text('Undiksha',style: TextStyle
+                                  (color: Colors.black54,
+                                    fontWeight: FontWeight.bold
+                                ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+  }
+}
